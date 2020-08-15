c71411f1c546fa65b2ca6c4341df64c285c689ad
cpress <cpress@it4ipm.de>
2020-08-15 16:27:10 +0200

    Introduction to Angular Attribute Directives

---
+++ src/app/directives/highlighted.directive.ts
@@ -0,0 +1,12 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[highlighted]'
+})
+export class HighlightedDirective {
+
+  constructor() {
+    console.log('Directive created..');
+  }
+
+}
