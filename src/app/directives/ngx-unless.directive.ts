636a7cafad1f7f2e9f25371218c0493d0b68be07
cpress <cpress@it4ipm.de>
2020-08-15 20:58:07 +0200

    Angular Structural Directives - Step-by-Step Implementation with attribute binding

--- src/app/directives/ngx-unless.directive.ts
+++ src/app/directives/ngx-unless.directive.ts
@@ -1,12 +1,25 @@
-import {Directive, ElementRef, TemplateRef, ViewContainerRef} from '@angular/core';
+import {Directive, ElementRef, Input, TemplateRef, ViewContainerRef} from '@angular/core';
 
 @Directive({
   selector: '[ngxUnless]'
 })
 export class NgxUnlessDirective {
 
+  // used to prevent unnecessary update cycles
+  visible = false;
+
   constructor(private template: TemplateRef<any>,
               private viewContainer: ViewContainerRef) {
   }
 
+  @Input()
+  set ngxUnless(condition: boolean) {
+    if (!condition && !this.visible) {
+      this.viewContainer.createEmbeddedView(this.template);
+      this.visible = true;
+    } else if (condition && this.visible) {
+      this.viewContainer.clear();
+      this.visible = false;
+    }
+  }
 }
