a41ce359f53b0e4a00c7c2055cee4f6a692d536d
cpress <cpress@it4ipm.de>
2020-08-22 22:28:16 +0200

    Angular AfterViewChecked Lifecycle Hook - Use Cases and Pitfalls

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,8 +1,8 @@
 import {
-  AfterContentChecked, AfterViewChecked,
+  AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
   Attribute,
   ChangeDetectionStrategy,
-  Component,
+  Component, DoCheck,
   EventEmitter,
   Input,
   OnChanges,
@@ -20,7 +20,8 @@ import {CoursesService} from '../services/courses.service';
   styleUrls: ['./course-card.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CourseCardComponent implements OnInit, OnDestroy, OnChanges, AfterContentChecked, AfterViewChecked {
+export class CourseCardComponent implements OnInit, OnDestroy, OnChanges, AfterContentChecked, AfterViewChecked, AfterContentInit,
+  AfterViewInit, DoCheck {
 
   @Input()
   course: Course;
@@ -33,6 +34,18 @@ export class CourseCardComponent implements OnInit, OnDestroy, OnChanges, AfterC
 
   constructor(private coursesService: CoursesService, @Attribute('type') private type: string) {
     console.log('constructor', this.course);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
   }
 
   // don't change properties of the view part but common dom ops like scrolling to bottom of list or something like that
